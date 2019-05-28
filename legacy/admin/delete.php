2111e966268690f278ed1c06ec9ba17abecec1f9
Michal Zbranek <michal.zbranek.1@gmail.com>
2019-05-28 15:57:06 +0200

    removed useless stuff

--- legacy/admin/delete.php
+++
@@ -1,6 +0,0 @@
-<?php
-include ("config.php");
-$id=$_GET["vase_id"];
-mysql_query("DELETE FROM clanky WHERE id='$id' LIMIT 1") or die(mysql_error());
-?>
-<A HREF="index.php">zpet</A>
--- legacy/admin/uvod.php
+++
@@ -1,4 +0,0 @@
-<A HREF="http://fcmalenovice.moxo.cz/admin/index.php?akce=odhlasit">odhlaseni uzivatele</A><BR>
-<A HREF="http://fcmalenovice.moxo.cz/admin/index.php?akce=pridat">pridani clanku</A><BR>
-<A HREF="http://fcmalenovice.moxo.cz/admin/index.php?akce=zobraz">zobrazeni clanku</A>
-
