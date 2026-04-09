cadd6e8f8a8dccc4c944adeff1044d177836d449
Michal Zbranek <michal.zbranek@koala42.com>
2026-04-09 12:56:18 +0200

    mobile hamburger

--- src/Horizontal.tsx
+++ src/Horizontal.tsx
@@ -2,13 +2,14 @@ import {
   AppBar,
   Box,
   Drawer,
+  IconButton,
   List,
   ListItem,
   ListItemButton,
   ListItemText,
   Toolbar,
-  Typography,
 } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
 function Horizontal({
   // @ts-ignore: Object is possibly 'null'.
@@ -40,10 +41,7 @@ function Horizontal({
 
   const drawer = (
     <Box onClick={handleDrawerToggle} className="text-center h-full bg-club-light">
-      <Typography variant="h6" className="my-4 font-russo text-club-blue">
-        FC MALENOVICE
-      </Typography>
-      <List>
+      <List className="pt-4">
         {navItems.map((item) => (
           <ListItem key={item.label} disablePadding>
             <ListItemButton
@@ -70,6 +68,15 @@ function Horizontal({
         sx={{ backgroundColor: '#0062A1' }}
       >
         <Toolbar className="justify-between container mx-auto px-4 py-2">
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ display: { xs: 'block', sm: 'none' }, mt: -1, ml: 1 }}
+          >
+            <MenuIcon />
+          </IconButton>
           <div
             className="font-russo text-2xl sm:text-3xl tracking-wider flex-grow sm:flex-grow-0 text-center sm:text-left"
           >
